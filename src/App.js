cc3612b1cb6d73c4b415a25c1ea855239bdbc9d2
3dzzh <1275354807@qq.com>
2019-11-18 11:16:32 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,209 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>建设行政</h1>
+      <table frame="box" rules="all">
+        <tr>
+          <th>序号</th>
+          <th>
+            处罚文书、证据资料等名称（按查办按键顺序形成的文书、资料分类列明）
+          </th>
+          <th>页号</th>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
